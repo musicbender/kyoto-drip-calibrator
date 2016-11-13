ef4ba5c6edab97280fa78a09ba33b22bcfefe55a
Pat Jacobs <musicbender@gmail.com>
2016-11-13 15:59:38 -0800

    more redux, material ui, and other project setups

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PlaybackControls from '../containers/playerback-controls.jsx';
+import Slider from '../containers/slider.jsx';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <PlaybackControls />
+          <Slider />
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReactPromise from 'react-promise';
 import App from './components/App.jsx';
+import recuers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const createStoreWithMiddleWare = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('app')
+);
