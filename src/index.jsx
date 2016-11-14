70907db648001a8567077ab40e22b9227f7f323b
Pat Jacobs <musicbender@gmail.com>
2016-11-13 19:33:23 -0800

    added redux sounds npm and audio file

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReactPromise from 'react-promise';
+import soundsMiddleware from 'redux-sounds';
 import App from './components/App.jsx';
 import recuers from './reducers';
 
-const createStoreWithMiddleWare = applyMiddleware(ReduxPromise)(createStore);
+const soundsData = { tickSound: './audio/tick.mp3' }
+const loadedSoundsMiddleware = soundsMiddleware(soundsData);
+const middleware = [ReduxPromise, loadedSoundsMiddleware];
+const createStoreWithMiddleWare = applyMiddleware(...middleware)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
