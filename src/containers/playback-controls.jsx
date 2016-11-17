a1ddd6b90291050bbe22e37b75008e603656e273
Pat Jacobs <musicbender@gmail.com>
2016-11-16 22:21:52 -0800

    build basic container with connected redux

--- src/containers/playback-controls.jsx
+++ src/containers/playback-controls.jsx
@@ -6,9 +6,30 @@ class PlaybackControls extends Component {
 
   }
 
+  handleTemp() {
+    if (this.props.playing) {
+      return "It's playing!";
+    } else if (!this.props.playing) {
+      return "It's not playing!";
+    } else {
+      "Uhh..."
+    }
+  }
+
+  handleClick() {
+    if (this.props.playing) {
+      this.props.soundOff();
+    } else {
+      this.props.soundOn();
+    }
+  }
+
   render() {
     return (
-
+      <div>
+        <div>{ handleTemp() }</div>
+        <button onClick={handleClick()}>Click Here</button>
+      </div>
     )
   }
 }
