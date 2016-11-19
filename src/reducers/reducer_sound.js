ace306c6f3c57e7fde3abcdb3e5000e55ade459d
Pat Jacobs <musicbender@gmail.com>
2016-11-18 23:17:10 -0800

    made oscillator creation function with a shorter sound

--- src/reducers/reducer_sound.js
+++ src/reducers/reducer_sound.js
@@ -8,21 +8,25 @@ const initialState = {
 export default function sound(state = initialState, action) {
     switch (action.type) {
         case SOUND_ON:
-            const osc = action.ctx.createOscillator();
-            const gain = action.ctx.createGain();
 
-            osc.type = 'sine';
-            osc.frequency.value = 2000;
-            gain.gain.value = 1;
-            osc.connect(gain);
-            gain.connect(action.ctx.destination);
-            osc.start();
+            function newOscillator() {
+                const osc = action.ctx.createOscillator();
+                const gain = action.ctx.createGain();
+                osc.type = 'sine';
+                osc.frequency.value = 2000;
+                gain.gain.value = 1;
+                osc.connect(gain);
+                gain.connect(action.ctx.destination);
+                osc.start();
+                osc.stop(action.ctx.currentTime + 0.15);
+            }
+
+            newOscillator();
+
             return {...state, playing: true, osc};
         case SOUND_OFF:
             var osc = state.osc;
 
-            osc.stop();
-            osc.disconnect();
             return {...state, playing: false, osc: null};
         default:
             return state;
